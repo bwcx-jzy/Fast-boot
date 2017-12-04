52b9903c4186027352f06ea82c5a76669735ab02
jiangzeyin <bwcx_jzy@163.com>
2017-12-04 13:31:47 +0800

    md 说明

--- common-boot/src/main/java/cn/jiangzeyin/pool/PoolConfig.java
+++ common-boot/src/main/java/cn/jiangzeyin/pool/PoolConfig.java
@@ -10,13 +10,18 @@ import java.util.concurrent.TimeUnit;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PoolConfig {
+    // 线程池核心数
     int value() default 0;
 
+    // 线程池最大线程数
     int maximumPoolSize() default Integer.MAX_VALUE;
 
+    // 线程空闲多久将销毁
     long keepAliveTime() default 60L;
 
+    // 时间单位
     TimeUnit UNIT() default TimeUnit.SECONDS;
 
+    // 线程池拒绝执行处理策略
     PolicyHandler HANDLER() default PolicyHandler.Caller;
 }
